9075f37ff3e84700710cbe34c590f3a723042cdf
Ruslan Reznick <reznick59@gmail.com>
2021-11-08 13:02:05 +0200

    second_commit

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,6 +3,6 @@ import * as ReactDOM from 'react-dom';
 
 
 window.addEventListener('load', () => {
-    ReactDOM.render(<Header />, document.getElementById('react-root'))
+    ReactDOM.render(<Header />, document.getElementById('react-root'));
 });
 
