9765fa54ccc48a4b92fcfe79b12115a7f0610915
Reznick <reznick59@gmail.com>
2021-11-21 17:01:13 +0200

    Add icons

--- src/shared/CardsList/Card/CardMenu/CardMenuList/CardMenuList.tsx
+++ src/shared/CardsList/Card/CardMenu/CardMenuList/CardMenuList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './cardmenulist.css';
+import {BlockIcon, ComentsIcon, ComplainIcon, SaveIcon, ShareIcon} from "../../../../Icons";
 
 interface IMenuItemListPost {
   postId: string;
@@ -8,11 +9,11 @@ interface IMenuItemListPost {
 export function CardMenuList({postId}: IMenuItemListPost) {
   return (
       <ul className={styles.cardMenuList}>
-        <li onClick={() => console.log(postId)} className={styles.menuItem}>Коментарии</li>
-        <li className={styles.menuItem}>Поделиться</li>
-        <li className={styles.menuItem}>Скрыть</li>
-        <li className={styles.menuItem}>Сохранить</li>
-        <li className={styles.menuItem}>Пожаловаться</li>
+        <li onClick={() => console.log(postId)} className={styles.menuItem}><ComentsIcon/> Коментарии</li>
+        <li className={styles.menuItem}><ShareIcon/> Поделиться</li>
+        <li className={styles.menuItem}><BlockIcon/> Скрыть</li>
+        <li className={styles.menuItem}><SaveIcon/> Сохранить</li>
+        <li className={styles.menuItem}><ComplainIcon/> Пожаловаться</li>
       </ul>
   );
 }
---
+++ src/shared/Icons/index.ts
@@ -0,0 +1,6 @@
+export * from './MenuIcon';
+export * from './BlockIcon';
+export * from './ComentsIcon';
+export * from './ComplainIcon';
+export * from './SaveIcon';
+export * from './ShareIcon';
